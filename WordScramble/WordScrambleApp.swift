de059bcf6f3a591fd5da5feec49e0fd35e4a1ee9
SilverRooney <silver.rooney.dev@gmail.com>
2022-05-10 12:58:27 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Silver on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
